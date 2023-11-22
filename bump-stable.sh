81ba009c3db67a2dcef68e1c6c4c25f93eb8aa1d
Innei <i@innei.in>
2023-11-22 13:02:30 +0800

    u
    Signed-off-by: Innei <i@innei.in>

--- bump-stable.sh
+++ bump-stable.sh
@@ -3,6 +3,7 @@ BASE=dev
 TO=stable
 
 git checkout $BASE
+git pull --rebase
 git checkout -B $HEAD
 git push -f origin $HEAD
 gh pr create --base $TO --head $HEAD --title "Bump to stable" --body "Bump" || exit 0
